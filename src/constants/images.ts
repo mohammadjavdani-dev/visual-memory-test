d1c47e9bd4b6221ba0491c677bbb4c95bfe21097
Mohammad Javdani <mohammadjavdani98@gmail.com>
2023-12-13 18:42:23 +0330

    add real images to assets directory

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,23 +1,362 @@
 const IMAGES_PATH = '/assets/images';
 
-// export const IMAGES = [
-//     {
-//         image_id: 0,
-//         image_src: `${IMAGES_PATH}/Robot.jpg`,
-//         image_title: 'ربات'
-//     }
-// ];
-
 type TImages = {
     image_id: number;
     image_src: string;
-    image_title: number;
+    image_title: string;
 }
 
-export const IMAGES: TImages[] = Array(70).fill(null).map((_: null, key) => ({
-    image_id: key + 1,
-    image_src: `${IMAGES_PATH}/Robot.jpg`,
-    image_title: key + 1
-}))
+export const IMAGES: TImages[] = [
+    {
+        image_id: 1,
+        image_src: `${IMAGES_PATH}/Ampoule.png`,
+        image_title: 'آمپول'
+    },
+    {
+        image_id: 2,
+        image_src: `${IMAGES_PATH}/Apple.png`,
+        image_title: 'سیب'
+    },
+    {
+        image_id: 3,
+        image_src: `${IMAGES_PATH}/Back-pack.png`,
+        image_title: "کوله پشتی"
+    },
+    {
+        image_id: 4,
+        image_src: `${IMAGES_PATH}/Baguette-Bread.png`,
+        image_title: "نان باگت"
+    },
+    {
+        image_id: 5,
+        image_src: `${IMAGES_PATH}/Bicycle.png`,
+        image_title: "دوچرخه"
+    },
+    {
+        image_id: 6,
+        image_src: `${IMAGES_PATH}/Bone.png`,
+        image_title: "استخوان"
+    },
+    {
+        image_id: 7,
+        image_src: `${IMAGES_PATH}/Boot.png`,
+        image_title: "چکمه"
+    },
+    {
+        image_id: 8,
+        image_src: `${IMAGES_PATH}/Brush.png`,
+        image_title: "قلم نقاشی"
+    },
+    {
+        image_id: 9,
+        image_src: `${IMAGES_PATH}/Cake.png`,
+        image_title: "کیک"
+    },
+    {
+        image_id: 10,
+        image_src: `${IMAGES_PATH}/Camera.png`,
+        image_title: "دوربین"
+    },
+    {
+        image_id: 11,
+        image_src: `${IMAGES_PATH}/Car.png`,
+        image_title: "ماشین"
+    },
+    {
+        image_id: 12,
+        image_src: `${IMAGES_PATH}/Cassette-Tape.png`,
+        image_title: "نوار کاست"
+    },
+    {
+        image_id: 13,
+        image_src: `${IMAGES_PATH}/Cheese.png`,
+        image_title: "پنیر"
+    },
+    {
+        image_id: 14,
+        image_src: `${IMAGES_PATH}/Cherry.png`,
+        image_title: "گیلاس"
+    },
+    {
+        image_id: 15,
+        image_src: `${IMAGES_PATH}/Chocolat.png`,
+        image_title: "شکلات"
+    },
+    {
+        image_id: 16,
+        image_src: `${IMAGES_PATH}/Clock.png`,
+        image_title: "ساعت"
+    },
+    {
+        image_id: 17,
+        image_src: `${IMAGES_PATH}/Cloud.png`,
+        image_title: "ابر"
+    },
+    {
+        image_id: 18,
+        image_src: `${IMAGES_PATH}/Color-bucket.png`,
+        image_title: "سطل رنگ"
+    },
+    {
+        image_id: 19,
+        image_src: `${IMAGES_PATH}/Controller.png`,
+        image_title: "دسته بازی"
+    },
+    {
+        image_id: 20,
+        image_src: `${IMAGES_PATH}/Diamond.png`,
+        image_title: "الماس"
+    },
+    {
+        image_id: 21,
+        image_src: `${IMAGES_PATH}/Dog.png`,
+        image_title: "سگ"
+    },
+    {
+        image_id: 22,
+        image_src: `${IMAGES_PATH}/Door.png`,
+        image_title: "درب"
+    },
+    {
+        image_id: 23,
+        image_src: `${IMAGES_PATH}/Fan.png`,
+        image_title: "پنکه"
+    },
+    {
+        image_id: 24,
+        image_src: `${IMAGES_PATH}/Faucet.png`,
+        image_title: "شیر آب"
+    },
+    {
+        image_id: 25,
+        image_src: `${IMAGES_PATH}/Fishbowl.png`,
+        image_title: "تنگ ماهی"
+    },
+    {
+        image_id: 26,
+        image_src: `${IMAGES_PATH}/Flower.png`,
+        image_title: "گل"
+    },
+    {
+        image_id: 27,
+        image_src: `${IMAGES_PATH}/Glasses.png`,
+        image_title: "عینک"
+    },
+    {
+        image_id: 28,
+        image_src: `${IMAGES_PATH}/Globe.png`,
+        image_title: "کره جغرافیایی"
+    },
+    {
+        image_id: 29,
+        image_src: `${IMAGES_PATH}/Gloves.png`,
+        image_title: "دستکش"
+    },
+    {
+        image_id: 30,
+        image_src: `${IMAGES_PATH}/Guitar.png`,
+        image_title: "گیتار"
+    },
+    {
+        image_id: 31,
+        image_src: `${IMAGES_PATH}/Hat.png`,
+        image_title: "کلاه"
+    },
+    {
+        image_id: 32,
+        image_src: `${IMAGES_PATH}/Headphone.png`,
+        image_title: "هدفون"
+    },
+    {
+        image_id: 33,
+        image_src: `${IMAGES_PATH}/Helicopter.png`,
+        image_title: "هلیکوپتر"
+    },
+    {
+        image_id: 34,
+        image_src: `${IMAGES_PATH}/Helmet.png`,
+        image_title: "کلاه ایمنی"
+    },
+    {
+        image_id: 35,
+        image_src: `${IMAGES_PATH}/Hourglass.png`,
+        image_title: "ساعت شنی"
+    },
+    {
+        image_id: 36,
+        image_src: `${IMAGES_PATH}/House.png`,
+        image_title: "خانه"
+    },
+    {
+        image_id: 37,
+        image_src: `${IMAGES_PATH}/Ice-Cream.png`,
+        image_title: "بستنی"
+    },
+    {
+        image_id: 38,
+        image_src: `${IMAGES_PATH}/Jacket.png`,
+        image_title: "ژاکت"
+    },
+    {
+        image_id: 39,
+        image_src: `${IMAGES_PATH}/Key.png`,
+        image_title: "کلید"
+    },
+    {
+        image_id: 40,
+        image_src: `${IMAGES_PATH}/Kite.png`,
+        image_title: "بادبادک"
+    },
+    {
+        image_id: 41,
+        image_src: `${IMAGES_PATH}/Knife.png`,
+        image_title: "چاقو"
+    },
+    {
+        image_id: 42,
+        image_src: `${IMAGES_PATH}/Lamp.png`,
+        image_title: "لامپ"
+    },
+    {
+        image_id: 43,
+        image_src: `${IMAGES_PATH}/Lantern.png`,
+        image_title: "فانوس"
+    },
+    {
+        image_id: 44,
+        image_src: `${IMAGES_PATH}/Leaf.png`,
+        image_title: "برگ"
+    },
+    {
+        image_id: 45,
+        image_src: `${IMAGES_PATH}/Letter-envelope.png`,
+        image_title: "پاکت نامه"
+    },
+    {
+        image_id: 46,
+        image_src: `${IMAGES_PATH}/Magnifier.png`,
+        image_title: "ذره بین"
+    },
+    {
+        image_id: 47,
+        image_src: `${IMAGES_PATH}/Measure-Tape.png`,
+        image_title: "متر"
+    },
+    {
+        image_id: 48,
+        image_src: `${IMAGES_PATH}/Medicine.png`,
+        image_title: "دارو"
+    },
+    {
+        image_id: 49,
+        image_src: `${IMAGES_PATH}/Milk.png`,
+        image_title: "شیر"
+    },
+    {
+        image_id: 50,
+        image_src: `${IMAGES_PATH}/Mixer.png`,
+        image_title: "مخلوط کن"
+    },
+    {
+        image_id: 51,
+        image_src: `${IMAGES_PATH}/Moon.png`,
+        image_title: "ماه"
+    },
+    {
+        image_id: 52,
+        image_src: `${IMAGES_PATH}/Motor-cycle.png`,
+        image_title: "موتور سیکلت"
+    },
+    {
+        image_id: 53,
+        image_src: `${IMAGES_PATH}/Nail-Clipper.png`,
+        image_title: "ناخن گیر"
+    },
+    {
+        image_id: 54,
+        image_src: `${IMAGES_PATH}/Orange.png`,
+        image_title: "پرتقال"
+    },
+    {
+        image_id: 55,
+        image_src: `${IMAGES_PATH}/Pot.png`,
+        image_title: "قابلمه"
+    },
+    {
+        image_id: 56,
+        image_src: `${IMAGES_PATH}/Reading-lamp.png`,
+        image_title: "چراغ مطالعه"
+    },
+    {
+        image_id: 57,
+        image_src: `${IMAGES_PATH}/Ruler.png`,
+        image_title: "خط‌کش"
+    },
+    {
+        image_id: 58,
+        image_src: `${IMAGES_PATH}/Safety-Pin.png`,
+        image_title: "سنجاق قفلی"
+    },
+    {
+        image_id: 59,
+        image_src: `${IMAGES_PATH}/Sandwich.png`,
+        image_title: "ساندویچ"
+    },
+    {
+        image_id: 60,
+        image_src: `${IMAGES_PATH}/Scissors.png`,
+        image_title: "قیچی"
+    },
+    {
+        image_id: 61,
+        image_src: `${IMAGES_PATH}/Scooter.png`,
+        image_title: "اسکوتر"
+    },
+    {
+        image_id: 62,
+        image_src: `${IMAGES_PATH}/Shoes.png`,
+        image_title: "کفش"
+    },
+    {
+        image_id: 63,
+        image_src: `${IMAGES_PATH}/Skimmer.png`,
+        image_title: "کفگیر"
+    },
+    {
+        image_id: 64,
+        image_src: `${IMAGES_PATH}/Slide.png`,
+        image_title: "سرسره"
+    },
+    {
+        image_id: 65,
+        image_src: `${IMAGES_PATH}/Socks.png`,
+        image_title: "جوراب"
+    },
+    {
+        image_id: 66,
+        image_src: `${IMAGES_PATH}/Suitcase.png`,
+        image_title: "چمدان"
+    },
+    {
+        image_id: 67,
+        image_src: `${IMAGES_PATH}/Teapot.png`,
+        image_title: "قوری"
+    },
+    {
+        image_id: 68,
+        image_src: `${IMAGES_PATH}/Telescope.png`,
+        image_title: "تلسکوپ"
+    },
+    {
+        image_id: 69,
+        image_src: `${IMAGES_PATH}/Tree.png`,
+        image_title: "درخت"
+    },
+    {
+        image_id: 70,
+        image_src: `${IMAGES_PATH}/Umbrella.png`,
+        image_title: "چتر"
+    }
+]
 
 export const CLONED_IMAGES: TImages[] = IMAGES.map(a => { return { ...a } });
