709c499ce9c83fe83ec2ba45db542f52cb83b11a
Mohammad Javdani <mohammadjavdani98@gmail.com>
2023-12-17 17:12:29 +0330

    complete second slide logic

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -1,22 +1,69 @@
 'use client'
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { Typography } from "@mui/material";
+import { CircularProgress, Typography } from "@mui/material";
 import { SlideContainer } from "@/components/templates";
 import { IMAGES } from "@/constants/images";
 import { useTimer } from "@/hooks";
 
+interface IState {
+    secondSlideContent: null | 'image' | 'text';
+    testParametherType: null | 'goal' | 'deflection' | 'neutral';
+    sourceImageId: null | number;
+    isMounted: boolean;
+}
+
 const SecondSlide = () => {
     useTimer({ slideNumber: 2, destination: '/third' });
 
-    return <SlideContainer>
-        {
-            false ?
-                <Typography variant="h1" sx={{ fontSize: "12rem" }}>{IMAGES[0].image_title}</Typography>
-                :
-                <Image src={IMAGES[0].image_src} width="512" height="512" alt="" />
+    const [state, setState] = useState<IState>({
+        secondSlideContent: null,
+        testParametherType: null,
+        sourceImageId: null,
+        isMounted: false
+    });
+
+    const { secondSlideContent, sourceImageId, testParametherType, isMounted } = state;
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const session = window.sessionStorage;
+            const currentTest = JSON.parse(session.getItem('tests') as string)[session.getItem('inprogress_test_number') as string];
+            setState({
+                secondSlideContent: currentTest.secondSlideContent,
+                testParametherType: currentTest.testParametherType,
+                sourceImageId: currentTest.sourceImageId,
+                isMounted: true
+            });
         }
-    </SlideContainer>
+    }, []);
+
+    const renderConditions = () => {
+        if (secondSlideContent === 'image') {
+            if (testParametherType === 'goal' || testParametherType === 'deflection') {
+                return <Image src={IMAGES[sourceImageId as number].image_src} width="512" height="512" alt="" />;
+            }
+        }
+
+        if (secondSlideContent === 'text') {
+            if (testParametherType === 'goal' || testParametherType === 'deflection') {
+                return <Typography variant="h1" sx={{ fontSize: "12rem" }}>{IMAGES[sourceImageId as number].image_title}</Typography>;
+            }
+        }
+
+        return null;
+    }
+
+    return <>
+        <SlideContainer>
+            {
+                !isMounted ?
+                    <CircularProgress size={72} /> :
+                    renderConditions()
+            }
+        </SlideContainer>
+    </>
 }
 
 export default SecondSlide;
